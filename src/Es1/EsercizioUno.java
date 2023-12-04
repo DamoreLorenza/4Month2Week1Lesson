3202c37fc15457dd8055665246e5bdb5d55de8c2
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-12-04 16:38:44 +0100

    mod

--- src/Es1/EsercizioUno.java
+++ src/Es1/EsercizioUno.java
@@ -1,2 +1,48 @@
-package Es1;public class EsercizioUno {
-}
+package Es1;
+import java.util.Random;
+import java.util.Scanner;
+public class EsercizioUno {
+    public static void main(String[] args) {
+
+        Random randomNumber = new Random();
+        int lunghezza = 5;
+        int[] array = new int[lunghezza];
+
+        for (int i = 0; i < lunghezza; i++) {
+            array[i] = randomNumber.nextInt(10) + 1;
+            System.out.print(array[i] + " ");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Inserisci un numero (0 per uscire): ");
+            int numeroInserito = scanner.nextInt();
+
+            if (numeroInserito == 0) {
+                break;
+            }
+
+            System.out.print("Inserisci la posizione dell'array (da 1 a 5): ");
+            int posizione = scanner.nextInt();
+
+            if (posizione >= 1 && posizione <= 5) {
+                array[posizione - 1] = numeroInserito;
+
+                System.out.print("Nuovo stato dell'array: ");
+                printArray(array);
+            } else {
+                System.out.println("Posizione non valida. Inserisci una posizione tra 1 e 5.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+    }
+
--- src/Es2/EsercizioDue.java
+++ src/Es2/EsercizioDue.java
@@ -1,2 +1,31 @@
-package Es2;public class EsercizioDue {
+package Es2;
+
+import java.util.Random;
+import java.util.Scanner;
+public class EsercizioDue {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Inserisci km percorsi: ");
+            int kmPercorsi = scanner.nextInt();
+            System.out.print("Inserisci litri carburante consumato: ");
+            int lConsumati = scanner.nextInt();
+
+            if (lConsumati == 0 && kmPercorsi != 0) {
+                throw new IllegalArgumentException("Il consumo di carburante non può essere pari a zero.");
+            }
+
+            double consumoMedio = (double) kmPercorsi / lConsumati;
+            System.out.println("Il consumo medio è: " + consumoMedio + " km/l");
+        } catch (Exception e) {
+            System.out.println("Si è verificato un errore: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
 }
+
